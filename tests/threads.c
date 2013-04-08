2946a7d7a173ed06ddb207bf694109f8de903b89
Arni <arnidat@gmail.com>
2013-04-08 03:59:18 +0200

    Reverted tests/thread.c to its original form

--- tests/threads.c
+++ tests/threads.c
@@ -6,23 +6,17 @@ static int threads[NUM_THREADS];
 static int A[NUM_THREADS];
 static volatile int done[NUM_THREADS];
 
-uint32_t gg;
-
 void test(int i)
 {
-  if (i==1 || i == 3) syscall_sleep(2000);
-  syscall_sem_p((usr_sem_t*)gg);
   printf("Thread %d doing its thing!\n", i);
   A[i] = i*2;
   done[i] = 1;
-  syscall_sem_v((usr_sem_t*)gg);
 }
 
 int main()
 {
   int i;
-  gg = 100;
-  syscall_sem_create((usr_sem_t*)gg,1);
+
   for (i = 0; i < NUM_THREADS; ++i)
   {
     A[i] = 0;
